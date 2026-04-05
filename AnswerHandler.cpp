855b2b38fe933cb040941409c74ff6a5e576952a
Zlata Pashko <zlatapashko@MacBook-Air-Zlata.local>
2026-04-05 13:52:20 +0300

    crated Answer Handler class

---
+++ AnswerHandler.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Zlata Pashko on 05.04.2026.
+//
+
+#include "AnswerHandler.h"
---
+++ AnswerHandler.h
@@ -0,0 +1,16 @@
+//
+// Created by Zlata Pashko on 05.04.2026.
+//
+
+#ifndef КУРСОВА_ANSWERHANDLER_H
+#define КУРСОВА_ANSWERHANDLER_H
+
+
+
+class AnswerHandler {
+
+};
+
+
+
+#endif //КУРСОВА_ANSWERHANDLER_H
